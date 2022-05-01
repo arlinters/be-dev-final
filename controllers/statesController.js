e30d47bea0af0e4292a2aec1ae62427089496753
Charles Love Winters III <charlie.winters17@gmail.com>
2022-05-01 11:13:23 -0500

    add additional state endpoints using JSON

--- controllers/statesController.js
+++ controllers/statesController.js
@@ -13,7 +13,43 @@ const getBySlug = (req, res) => {
 	// res.json(result[0]);
 }
 
+const getCapital= (req, res) => {
+	const result = data.states.find(x => x.code === req.code);
+	res.json({
+		"capital_city": result.capital_city,
+		"capital_url": result.capital_url
+	});
+}
+
+const getAdmission = (req, res) => {
+	const result = data.states.find(x => x.code === req.code);
+	res.json({
+		"admission_date": result.admission_date,
+		"admission_number": result.admission_number
+	});
+}
+
+const getPopulation = (req, res) => {
+	const result = data.states.find(x => x.code === req.code);
+	res.json({
+		"population": result.population,
+		"population_rank": result.population_rank
+	});
+}
+
+const getNickname = (req, res) => {
+	const result = data.states.find(x => x.code === req.code);
+	
+	res.json({
+		"nickname": result.nickname,
+	});
+}
+
 module.exports = {
 	getAllStates,
-	getBySlug
+	getBySlug,
+	getCapital,
+	getAdmission,
+	getPopulation,
+	getNickname
 }
--- routes/api/states.js
+++ routes/api/states.js
@@ -9,7 +9,10 @@ router.route('/')
     // .put(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), employeesController.updateEmployee)
     // .delete(verifyRoles(ROLES_LIST.Admin), employeesController.deleteEmployee);
 
-// router.use()
 router.route('/:slug').get(verifyStates, statesController.getBySlug);
+router.route('/:slug/capitol').get(verifyStates, statesController.getCapital)
+router.route('/:slug/admission').get(verifyStates, statesController.getAdmission)
+router.route('/:slug/population').get(verifyStates, statesController.getPopulation)
+router.route('/:slug/nickname').get(verifyStates, statesController.getNickname)
 
 module.exports = router;
